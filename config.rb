3095540662dea36006137952b8e3585a87af455a
Fernando Moreira <nandomoreira.me@gmail.com>
2015-10-06 22:46:34 -0300

    remove css folder, add Gemfile, add Bower...

--- config.rb
+++ config.rb
@@ -2,8 +2,8 @@ require 'compass/import-once/activate'
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
-http_path     = "/"
-css_dir       = "css"
-sass_dir      = "scss"
-output_style  = :compact # :expanded or :nested or :compact or :compressed
+http_path = "./"
+css_dir = "./"
+sass_dir = "scss/"
+output_style = :compact # :expanded or :nested or :compact or :compressed
 line_comments = false
